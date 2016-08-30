ec2028d87fca116dfd902bd38949ee6fb2878f90
msawangwan <misha.sawangwan@gmail.com>
2016-08-29 18:17:40 -0700

    refactored some stuff

--- Assets/Scripts/FollowOther.cs
+++ Assets/Scripts/FollowOther.cs
@@ -25,32 +25,6 @@ public class FollowOther : MonoBehaviour {
 		}
 	}
 
-	void PaintRay2D () {
-		Debug.DrawRay ( transform.position, transform.up * 3.0f, Color.yellow, 0.2f, false );
-	}
-
-	void FollowMouseSmoothDamp ( GameObject self, float transitionSpeed = 0.25f, float z = 10.0f ) {
-
-		PaintRay2D ();
-
-		Vector3 p1 = self.transform.position;
-		Vector3 p2 = Camera.main.ScreenToWorldPoint ( new Vector3 ( Input.mousePosition.x, Input.mousePosition.y, z ) );
-		Vector3 vel = Vector3.zero;
-
-		self.transform.position = Vector3.SmoothDamp ( p1, p2, ref vel, transitionSpeed );
-	}
-
-	void FollowMouseLerp ( GameObject self, float transitionSpeed = 0.5f, float startTime = 2.5f, float z = 10.0f ) {
-
-		Vector3 p1 = self.transform.position;
-		Vector3 p2 = Camera.main.ScreenToWorldPoint ( new Vector3 ( Input.mousePosition.x, Input.mousePosition.y, z ) );
-	
-		float t = ( ( Time.time - startTime ) ) * transitionSpeed;
-		float d = Distance ( p1, p2 );
-
-		self.transform.position = Vector3.Lerp ( p1, p2, ( t / d ) );
-	}
-
 	void Interpolate () {
 
 		isInterpolating = true;
@@ -64,37 +38,49 @@ public class FollowOther : MonoBehaviour {
 
 		PaintRay2D ();
 
-		float percent = ExpStep ( TimeStep ( t ) );
+		float percent = ExpStepf ( TimeStepf ( t ) );
 		self.position = Vector3.Lerp ( start, end, percent );
 
 		if ( percent >= 1.0f ) return true;
 		else return false;
 	}
 
+	Vector3 TurnTo ( Vector3 a, Vector3 b ) {
+		float theta = Mathf.Atan2 ( a.)
+	}
+
 	/* t = currentLerpTime / lerpTime */
-	float TimeStep ( float t ) {
+	float TimeStepf ( float t ) {
 		return ( Time.time - t ) / 1.0f;
 	}
 
 	/* t = t * t * ( 3 - ( 2 * t ) ) */
-	float SmoothStep ( float t ) {
+	float SmoothStepf ( float t ) {
 		return ( t * t ) * ( 3.0f - ( 2.0f * t ) );
 	}
 
-	float SinStep ( float t ) {
+	float SinStepf ( float t ) {
 		return Mathf.Sin ( t * Mathf.PI * 0.5f );
 	}
 
-	float CosStep ( float t ) {
+	float CosStepf ( float t ) {
 		return 1 - Mathf.Cos ( t * Mathf.PI * 0.5f );
 	}
 
-	float ExpStep ( float t ) {
+	float ExpStepf ( float t ) {
 		return t * t;
 	}
 
-	/* d = sqrt ( (x2-x1) + (y2-y1) ) */
-	float Distance ( Vector3 a, Vector3 b ) {
+	/* d = sqrt ( (x2-x1) + (y2-y1) ) = magnitude */
+	float Distancef ( Vector3 a, Vector3 b ) {
 		return Mathf.Sqrt( ( ( b.x - a.x ) * ( b.x - a.x ) ) + ( ( b.y - a.y ) * ( b.y - a.y ) ) );
+	}
+
+	float Magnitudef ( Vector3 a, Vector3 b, float z ) {
+		return new Vector3( ( b.x - a.x ) * ( b.x - a.x ) ), ( ( b.y - a.y ) * ( b.y - a.y ), 2.0f );
+	}
+
+	void PaintRay2D () {
+		Debug.DrawRay ( transform.position, transform.up * 3.0f, Color.yellow, 0.2f, false );
 	}
 }
